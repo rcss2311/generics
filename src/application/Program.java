8d2173ab4fc2c5820eb0726a353f8ad6cebb677b
Ricardo Souza <rcss2311@gmail.com>
2020-11-10 18:31:10 -0300

    Object print service

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
+import services.PrintService;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		PrintService ps = new PrintService();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		Integer x = (Integer) ps.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
 	}
-
 }
